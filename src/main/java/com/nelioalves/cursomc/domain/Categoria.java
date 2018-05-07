55ac43e444802ceda93efdd1cc1a7f68a2c55048
“Paulo <pcnandes@yahoo.com.br>
2018-05-07 16:39:13 -0300

    Configurando banco H2 e JPA

--- src/main/java/com/nelioalves/cursomc/domain/Categoria.java
+++ src/main/java/com/nelioalves/cursomc/domain/Categoria.java
@@ -1,10 +1,19 @@
 package com.nelioalves.cursomc.domain;
 
 import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+
+@Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	//IDENTITY funciona bem com H@, mas dependendo do banco tem que alterar (sequence por ex.)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
