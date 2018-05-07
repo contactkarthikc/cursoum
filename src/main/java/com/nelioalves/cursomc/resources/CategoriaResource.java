07a7debd96ebc99ca3d7ee0eec4a118310bc0950
“Paulo <pcnandes@yahoo.com.br>
2018-05-07 16:23:27 -0300

    Testando categoria

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.nelioalves.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nelioalves.cursomc.domain.Categoria;
 
 //indica que uma classe rest
 @RestController
@@ -12,8 +17,16 @@ public class CategoriaResource {
 
 	//indico a chamada HTML
 	@RequestMapping(method=RequestMethod.GET)
-	private String listar() {
-		return "Rest está funcionando!";
+	private List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 	
 }
