4c8e67ca932709aa8a3c9c5928e5c9eb22d83d50
“Paulo <pcnandes@yahoo.com.br>
2018-05-16 11:02:17 -0300

    Criando profile de teste

--- src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
+++ src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
@@ -31,7 +31,11 @@ public class ItemPedido  implements Serializable {
 		this.quantidade = quantidade;
 		this.preco = preco;
 	}
-
+	
+	public double getSubTotal() {
+		return (this.preco-this.desconto)*this.quantidade;
+	}
+	
 	// ignora a serialização
 	// tudo que começa com get é serializado
 	@JsonIgnore
