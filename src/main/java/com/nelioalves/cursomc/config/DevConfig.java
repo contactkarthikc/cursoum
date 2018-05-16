adb45a8e5b466caa2f71dacfe1271764b0e0bf3a
“Paulo <pcnandes@yahoo.com.br>
2018-05-16 11:27:09 -0300

    Criando profile de desenvolvimento

---
+++ src/main/java/com/nelioalves/cursomc/config/DevConfig.java
@@ -0,0 +1,37 @@
+package com.nelioalves.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.nelioalves.cursomc.services.DBService;
+
+
+// classe de configuração que indica que todos os Beans que estiverem dentro dessa classe
+// ficaram ativos quando o profile test estiver ativo para o sistema. 
+// application.properties -> spring.profiles.active=test
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	//recupera uma propriedade descrita no progile ativo definido em application.properties
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		
+		if(!"create".equals(strategy)) {
+			// método responsável por instanciar os objetos e incluí-los na base de testes
+			dbService.instantiateTestDatabase();
+		}
+		return true;
+	}
+}
